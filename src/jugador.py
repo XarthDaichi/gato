55bcf0fdae2d2e6618dbb11e351d765bfd0b8ee6
Diego <74204361+XarthDaichi@users.noreply.github.com>
2022-11-23 22:01:54 -0600

    creating Player hierarchy PlayerHuman and PlayerCPu

--- src/jugador.py
+++ src/jugador.py
@@ -1,5 +1,24 @@
-class Jugador:
-    def __int__(self):
-        self.nombre = None
-        self.x = None
+import dificultad
 
+class Jugador:
+    def __int__(self, nombre, letra):
+        self.nombre = nombre
+        self.letra = letra
+
+    def movida(self):
+        pass
+
+class JugadorHumano(Jugador):
+    def __init__(self, nombre, letra):
+        super().__init__(nombre, letra)
+
+    def movida(self):
+        pass
+
+class JugadorCPU(Jugador):
+    def __init__(self, letra, dificultad):
+        super().__init__('CPU', letra)
+        self.dificultad = dificultad
+
+    def movida(self):
+        pass
