081e97ae40b7814f32588bfc9ecb89d4051b76c4
Diego <74204361+XarthDaichi@users.noreply.github.com>
2022-11-23 22:02:16 -0600

    creating the desicionTree for minimax

--- src/arbol_decisiones.py
+++ src/arbol_decisiones.py
@@ -1,3 +1,14 @@
+class Nodo: # sirve como los posibles estados del tablero para el algoritmo minimax
+    def __init__(self, valor):
+        self.valor = valor # tablero = [0,1,2,3,4,5,6,7,8]
+        self.siguientes = None
+
 class ArbolDecisiones:
     def __int__(self):
-        self.something = None
+        self.root = None
+
+    def insertar(self, valor):
+        if self.root is None:
+            self.root = Nodo(valor)
+            return
+
