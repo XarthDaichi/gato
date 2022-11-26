83363dc21d1b21e7b36777abee7edb8c652c2ee6
MauRiceV7 <mauricevv07@gmail.com>
2022-11-26 14:59:42 -0600

    Test jugadorHumano movida

--- tst/jugador_test.py
+++ tst/jugador_test.py
@@ -11,7 +11,14 @@ class JugadorHumanoTestCase(unittest.TestCase):
         self.assertEqual("X", jugadorTest.letra)
         self.assertEqual("", jugadorTest2.nombre)
         self.assertEqual("", jugadorTest2.letra)
-
+    def test_movida(self):
+        jugadorTest = jugador.JugadorHumano("Juan","X")
+        tablero = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+        tablero2 = jugadorTest.movida(tablero, 1)
+        tablero3 = jugadorTest.movida(tablero, 1)
+        self.assertEqual(tablero2[0], "X")
+        #prueba que al intentar insertar un una posicion ya insertada devuelva el mismo tablero
+        self.assertEqual(tablero2[0],tablero3[0])
 class JugadorCpuTestCase(unittest.TestCase):
     def test_constructor(self):
         jugadorTest = jugador.JugadorCPU("X", 1)
