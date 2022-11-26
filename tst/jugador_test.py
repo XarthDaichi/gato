afcaf0fdacc48fcb29ec9c17d64be56cdc318fac
MauRiceV7 <mauricevv07@gmail.com>
2022-11-26 14:00:05 -0600

    Update tests de jugadores

--- tst/jugador_test.py
+++ tst/jugador_test.py
@@ -3,16 +3,22 @@ from src import jugador
 
 
 class JugadorHumanoTestCase(unittest.TestCase):
-    def test_herencia(self):
+    def test_constructor(self):
         jugadorTest = jugador.JugadorHumano("Juan", 'X')
+        jugadorTest2 = jugador.JugadorHumano()
+
         self.assertEqual("Juan", jugadorTest.nombre)
         self.assertEqual("X", jugadorTest.letra)
+        self.assertEqual("", jugadorTest2.nombre)
+        self.assertEqual("", jugadorTest2.letra)
 
 class JugadorCpuTestCase(unittest.TestCase):
-    def test_herencia(self):
+    def test_constructor(self):
         jugadorTest = jugador.JugadorCPU("X", 1)
         self.assertEqual("CPU", jugadorTest.nombre)
-        self.assertEqual("", jugadorTest.letra)
+        self.assertEqual("X", jugadorTest.letra)
+        self.assertEqual(1, jugadorTest.dificultad)
+
 
 if __name__ == '__main__':
     unittest.main()
